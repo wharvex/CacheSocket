236a075e1fab97267d22aa5c8aec6dfe095b740f
wharvex <t.gudlewski@gmail.com>
2024-10-13 11:06:09 -0400

    fix: remove variable from run script

--- run.zsh
+++ run.zsh
@@ -4,16 +4,16 @@
 find . -name "*.java" -print | xargs astyle -q
 
 # find and delete backups that astyle creates
-a=$(find . -name "*.orig" -print)
-b=$(echo -e "$a" | wc -l)
-if [ $b -gt 0 ]
+a=$(find . -name "*.orig" -print | wc -l)
+if [ $a -gt 0 ]
 then
-  echo -e "$a" | xargs rm
+  find . -name "*.orig" -print | xargs rm
 fi
 
 # compile
 find . -name "*.java" -print | xargs javac -d out
 
 # run
-java -cp out client.client 3 4 5 6 7
 java -cp out server.server 3 4
+sleep 1
+java -cp out client.client 3 4 5 6 7
