4e408da6ae53d49575072b9f84778f2ab56f9480
wharvex <t.gudlewski@gmail.com>
2024-10-14 22:28:58 -0400

    chore: client socket
    Start setting up the client socket code properly.

--- client/client.java
+++ client/client.java
@@ -5,12 +5,17 @@ import java.net.*;
 import tcp.tcp_transport;
 import java.util.Scanner;
 import static utils.utils.tryParsePort;
+import java.nio.file.*;
+import java.nio.charset.*;
 
 public class client {
     public static void main(String[] args) throws Exception {
+        // Testing.
         System.out.println("Hello from client!");
         tcp_transport x = new tcp_transport();
         x.announce();
+
+        // Validate/store command line args.
         if (args.length != 5) {
             throw new IllegalArgumentException("Run client with exactly 5 arguments.");
         }
@@ -19,27 +24,38 @@ public class client {
         String cacheIP = args[2];
         int cachePort = tryParsePort(args[3]);
         String transportProtocol = args[4];
+
+        // Testing.
         System.out.println(serverIP + serverPort + cacheIP + cachePort + transportProtocol);
+
+        // Accept/process user commands.
         Scanner s = new Scanner(System.in);
         String currentLine;
         do {
+            // Accept line of user input.
             currentLine = s.nextLine();
+
+            // Get the first word.
             String splitLine[] = currentLine.split(" ", 2);
             String cmdType = splitLine[0];
+
+            // Testing
+            String currentPath = new java.io.File(".").getCanonicalPath();
+            System.out.println("current path: " + currentPath);
+
+            // Proceed according to the command type.
             switch (cmdType) {
             case "get":
-                System.out.println("this is a get command");
-                String sentence;
-                String modifiedSentence;
+                System.out.println("get");
                 BufferedReader inFromUser = new BufferedReader(
                     new InputStreamReader(System.in));
-                Socket clientSocket = new Socket(serverIP, serverPort);
-                DataOutputStream outToServer = new DataOutputStream(
+                Socket clientSocket = new Socket(cacheIP, cachePort);
+                DataOutputStream toServer = new DataOutputStream(
                     clientSocket.getOutputStream());
-                BufferedReader inFromServer =
+                BufferedReader fromServer =
                 new BufferedReader(new InputStreamReader(
                                        clientSocket.getInputStream()));
-                sentence = inFromUser.readLine();
+                sentence = fromUser.readLine();
                 outToServer.writeBytes(sentence + '\n');
                 modifiedSentence = inFromServer.readLine();
                 System.out.println("FROM SERVER: " +
@@ -47,7 +63,11 @@ public class client {
                 clientSocket.close();
                 break;
             case "put":
-                System.out.println("this is a put command");
+                System.out.println("put");
+                Path path = FileSystems.getDefault().getPath("client_fl",
+                        splitLine[1]);
+                BufferedReader reader = Files.newBufferedReader(path,
+                        StandardCharsets.UTF_8);
                 break;
             case "quit":
                 System.out.println("Exiting program!");
